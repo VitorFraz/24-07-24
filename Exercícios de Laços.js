43654932df049a2bcb2b09424ec9a4129f4c5683
Fraz <vfbarduco@gmail.com>
2024-08-01 14:33:31 -0300

    Add files via upload

---
+++ Exercícios de Laços.js
@@ -0,0 +1,96 @@
+contador = 0
+contador_02 = 0
+contador_03 = 0
+contador_04 = 1
+contador_05 = 0
+contador_06 = 0
+
+console.log("--------EX 1--------")
+
+while (contador < 10)
+{
+
+    contador = contador + 1
+    console.log(contador)
+
+}
+
+console.log("--------EX 2--------")
+
+while (contador_02 < 20)
+{
+
+    contador_02 = contador_02 + 2
+    console.log(contador_02)
+
+}
+
+console.log("--------EX 3--------")
+
+//while (contador_03 <= 100)
+{
+    //resultado = contador_03 + 1
+    //console.log(resultado)
+}
+
+console.log("--------EX 4--------")
+
+while (contador_04 <= 100)
+{
+    
+    resultado = contador_04 * 5
+
+    console.log( 5 , "x" , contador_04 , "=" , resultado)
+    contador_04 = contador_04 + 1
+
+}
+
+console.log("--------EX 5--------")
+
+while (contador_05 < 10)
+{
+
+    contador_05 = contador_05 + 1
+    console.log("Eu estou aprendendo laços de repetição")
+
+}
+
+console.log("--------EX 6--------")
+
+contador_06 = 6
+resultado = 1
+count = 1
+  
+    while(count<=contador_06){
+     resultado *= count
+     count++
+    }
+  
+    console.log(resultado)
+
+console.log("--------EX 7--------")
+
+termo = ('numero')
+resposta = ('34')
+penultimo = 0 , ultimo = 1
+
+if (termo <=2 )
+ numero = termo -1
+else{
+ count = 3
+ while (count <= termo) {
+  numero = ultimo + penultimo
+  penultimo = ultimo
+  ultimo =numero
+  count++
+ }
+}
+
+console.log(resposta)
+
+console.log("--------EX 8---------")
+
+
+
+
+
